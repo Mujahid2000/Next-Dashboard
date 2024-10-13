7395ce77f7bdc4b6c129060ac13f4e77963bef8d
Mujahid2000 <your.email@example.com>
2024-10-13 20:21:28 +0600

    add some new code 1

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -61,74 +61,72 @@ const Page = ({ children }) => {
 
   return (
     <PrivateRoute>
-      <div className="flex flex-col h-screen">
-        
-        <header className="bg-[#2E2E48] h-20 fixed w-full z-50 flex items-center justify-between px-4 md:px-8 shadow-md">
-          <div className="flex items-center space-x-2">
-            <img
-              className="w-9 h-9"
-              src="https://i.ibb.co/bzwmrds/Group-302.png"
-              alt="logo"
-            />
-            <h1 className="text-xl text-white">Dashboard</h1>
-          </div>
-          <p className="text-white">{userData?.name}</p>
+  <div className="flex flex-col h-screen">
+    
+    <header className="bg-[#2E2E48] h-20 fixed w-full z-50 flex items-center justify-between px-4 md:px-8 shadow-md">
+      <div className="flex items-center space-x-2">
+        <img
+          className="w-9 h-9"
+          src="https://i.ibb.co/bzwmrds/Group-302.png"
+          alt="logo"
+        />
+        <h1 className="text-xl text-white">Dashboard</h1>
+      </div>
+      <p className="text-white">{userData?.name}</p>
 
-          
-          <button
-            className="text-white 2xl:hidden focus:outline-none"
-            ref={toggleButtonRef}
-            onClick={toggleSidebar}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-        </header>
+      <button
+        className="text-white 2xl:hidden focus:outline-none"
+        ref={toggleButtonRef}
+        onClick={toggleSidebar}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+    </header>
 
-        
-        <div className="flex mt-20 flex-1">
-          {/* Sidebar */}
-          <aside
-          ref={sidebarRef}
-            className={`z-40 flex flex-col justify-between md:w-64 w-64 fixed h-full transform transition-transform duration-300 ease-in-out bg-[#2E2E48] shadow-lg ${
-              openMenu ? 'translate-x-0' : '-translate-x-full'
-            } 2xl:translate-x-0`}
-          >
-            <div className="flex flex-col h-full">
-             
-              <nav className="mt-10 flex-grow">
-                <Link href="/dashboard/overview">
-                  <p className="block py-2.5 px-4 text-white hover:bg-[#475BE8] cursor-pointer">
-                    Overview
-                  </p>
-                </Link>
-                <Link href="/dashboard/order">
-                  <p className="block py-2.5 px-4 text-white hover:bg-[#475BE8] cursor-pointer">
-                    Orders
-                  </p>
-                </Link>
-              </nav>
+    <div className="flex mt-20 flex-1">
+      {/* Sidebar */}
+      <aside
+        ref={sidebarRef}
+        className={`z-40 flex flex-col justify-between md:w-72 w-64 fixed h-full transform transition-transform duration-300 ease-in-out bg-[#2E2E48] shadow-lg ${
+          openMenu ? 'translate-x-0' : '-translate-x-full'
+        } 2xl:translate-x-0`}
+      >
+        <div className="flex flex-col h-full">
+          <nav className="mt-10 flex-grow">
+            <Link href="/dashboard/overview">
+              <p className="block py-2.5 px-4 text-white hover:bg-[#475BE8] cursor-pointer">
+                Overview
+              </p>
+            </Link>
+            <Link href="/dashboard/order">
+              <p className="block py-2.5 px-4 text-white hover:bg-[#475BE8] cursor-pointer">
+                Orders
+              </p>
+            </Link>
+          </nav>
 
-              {/* Logout Button */}
-              <div className="p-4 mb-20">
-                <button
-                  className="w-full py-2 px-4 bg-red-600 hover:duration-300 text-white rounded hover:bg-red-700"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </button>
-              </div>
-            </div>
-          </aside>
-
-          {/* Main content */}
-          <div className="flex-1 bg-[#383854] p-4 md:p-6 max-w-6xl 2xl:max-w-7xl mx-auto">
-            {children}
+          {/* Logout Button */}
+          <div className="p-4 mb-20">
+            <button
+              className="w-full py-2 px-4 bg-red-600 hover:duration-300 text-white rounded hover:bg-red-700"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
           </div>
         </div>
+      </aside>
+
+      {/* Main content */}
+      <div className="flex-1 bg-[#383854] p-4 md:p-6  2xl:max-w-7xl mr-auto lg:ml-20 xl:80 xl:ml-96">
+        {children}
       </div>
-    </PrivateRoute>
+    </div>
+  </div>
+</PrivateRoute>
+
   );
 };
 
--- app/dashboard/overview/page.js
+++ app/dashboard/overview/page.js
@@ -106,9 +106,9 @@ const Page = () => {
   };
 
   return (
-    <div className="p-6 min-h-screen">
+    <div className="p-6">
       <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3  gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3  gap-6">
         {/* Line Chart */}
         <div className="shadow-lg p-4 rounded-lg">
           <h2 className="text-lg font-semibold mb-4">Sales Over Time (Line Chart)</h2>
